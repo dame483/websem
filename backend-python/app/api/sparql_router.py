e2f2bae9de7c8ee2ebfa04617236931e3486cd4d
Dame DIENG <diengdame483@gmail.com>
2026-01-14 06:00:57 +0100

    Rebase & first endpoint python

--- backend-python/app/api/sparql_router.py
+++ backend-python/app/api/sparql_router.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
 from app.models.schemas import SparqlResponse, SparqlRequest
+from app.services.openai_services import generate_sparql as generate_sparql_query
 
 router = APIRouter()
 
-@router.post("/sparql",response_model=SparqlResponse)
+@router.post("/",response_model=SparqlResponse)
 def generate_sparql(request: SparqlRequest):
-    sparql = generate_sparql(request.sentence)
+    sparql = generate_sparql_query(request.sentence)
     return {"sparql": sparql}
