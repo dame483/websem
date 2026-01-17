cadb11eb52d7f3e731e7e8254e49f0f0c7fbc1b0
miich <mohammed.iich@insa-lyon.fr>
2026-01-17 23:39:40 +0100

    Front et back adapté

--- backend-java/src/main/java/fr/insalyon/websem/service/MovieExplorationSPARQLService.java
+++ backend-java/src/main/java/fr/insalyon/websem/service/MovieExplorationSPARQLService.java
@@ -34,6 +34,23 @@ public class MovieExplorationSPARQLService {
         return movies;
     }
 
+    // Recherche de films avec filtres avancés
+    public List<Movie> searchMoviesWithFilters(String title, String language, String country, 
+                                               String director, String producer, String yearFrom, 
+                                               String yearTo, String distributor) {
+        String sparqlQuery = buildAdvancedSearchQuery(title, language, country, director, producer, 
+                                                      yearFrom, yearTo, distributor);
+        ResultSet results = executeSparqlQuery(sparqlQuery);
+
+        List<Movie> movies = new ArrayList<>();
+        if (results != null) {
+            while (results.hasNext()) {
+                movies.add(mapSolutionToMovie(results.nextSolution()));
+            }
+        }
+        return movies;
+    }
+
     /**
      * Construit la requête SPARQL de recherche de films.
      * La requête récupère les principales informations du film :
@@ -453,10 +470,30 @@ public class MovieExplorationSPARQLService {
     private String getStringValue(QuerySolution solution, String varName) {
         if (solution.contains(varName)) {
             String value = solution.get(varName).toString();
+            // Nettoyer les suffixes de type RDF (^^http://...)
+            if (value.contains("^^http")) {
+                value = value.substring(0, value.indexOf("^^http"));
+            }
+            // Nettoyer les suffixes de langue (@en, @fr, etc.)
             if (value.contains("@")) {
                 value = value.substring(0, value.lastIndexOf("@"));
             }
-            return value;
+            
+            // Nettoyer les URIs enchâssées (ex: "Bengali, http://dbpedia.org/resource/Bengali_language")
+            // Supprimer tout ce qui ressemble à une URI DBpedia après la virgule et l'espace
+            value = value.replaceAll(", http://dbpedia\\.org/resource/[^\\s,]*", "");
+            
+            // Nettoyer les URIs pour récupérer juste le nom final (au cas où il en reste)
+            if (value.startsWith("http://dbpedia.org/resource/")) {
+                value = value.replace("http://dbpedia.org/resource/", "").replace("_", " ");
+            }
+            
+            // Nettoyer les espaces en double et les virgules mal placées
+            value = value.replaceAll(",\\s*,", ",")  // Supprimer les virgules doubles
+                    .replaceAll("^[,\\s]+|[,\\s]+$", "")  // Supprimer les virgules/espaces au début/fin
+                    .replaceAll("\\s+", " ");  // Normaliser les espaces
+            
+            return value.trim();
         }
         return null;
     }
@@ -496,12 +533,12 @@ public class MovieExplorationSPARQLService {
         movie.setEditor(getStringValue(solution, "editors"));
         movie.setStudio(getStringValue(solution, "studios"));
         movie.setMusicComposer(getStringValue(solution, "musicComposers"));
-        movie.setRuntime(getStringValue(solution, "runtime"));
+        movie.setRuntime(formatRuntime(getStringValue(solution, "runtime")));
         movie.setDistributor(getStringValue(solution, "distributors"));
         movie.setCountry(getStringValue(solution, "countries"));
         movie.setLanguage(getStringValue(solution, "languages"));
-        movie.setGross(getStringValue(solution, "gross"));
-        movie.setBudget(getStringValue(solution, "budget"));
+        movie.setGross(formatCurrency(getStringValue(solution, "gross")));
+        movie.setBudget(formatCurrency(getStringValue(solution, "budget")));
         movie.setThumbnail(getStringValue(solution, "thumbnail"));
         return movie;
     }
